faac705cf77f9d16274c4a06f1a505d8d2e723af
Dextrous <ssjdexucyche@gmail.com>
2019-11-07 16:40:45 +0530

    Updates

---
+++ precache-manifest.1c32603510c421f060df537843a38a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62c60143ff556503677a632b31abacf",
+    "url": "/dadjokesreact/index.html"
+  },
+  {
+    "revision": "bfafdc754a4d4aee6aa6",
+    "url": "/dadjokesreact/static/css/main.698d90e2.chunk.css"
+  },
+  {
+    "revision": "9adc5ae7c3e6be80c531",
+    "url": "/dadjokesreact/static/js/2.d1e7e2c9.chunk.js"
+  },
+  {
+    "revision": "bfafdc754a4d4aee6aa6",
+    "url": "/dadjokesreact/static/js/main.f3795eea.chunk.js"
+  },
+  {
+    "revision": "0f3ae6e63e0df0c97ae2",
+    "url": "/dadjokesreact/static/js/runtime-main.023bcc14.js"
+  },
+  {
+    "revision": "fd8a6e42bc76d9433b2d4601e018e022",
+    "url": "/dadjokesreact/static/media/emoji.fd8a6e42.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/dadjokesreact/static/media/logo.25bf045c.svg"
+  }
+]);
